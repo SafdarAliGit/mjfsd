2322e01e339bd88252029cf7ccf8ae57c656509d
SafdarAliGit <safdar211@gmail.com>
2025-05-19 17:29:04 +0500

    updated

--- mjfsd/loom_production/doctype/loom_production/loom_production.py
+++ mjfsd/loom_production/doctype/loom_production/loom_production.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class LoomProduction(Document):
-	pass
+	def before_save(self):
+		if self.shift_working_hours:
+			for row in self.loom_production_items:
+				if row.rpm:
+					unit_per_rpm = round((row.rpm * (0.72 / 12)) * self.shift_working_hours)
+					row.unit_per_rpm = unit_per_rpm
+
+					if row.actual_reading:
+						try:
+							row.effeciency = int((row.actual_reading / unit_per_rpm) * 100)
+						except ZeroDivisionError:
+							row.effeciency = 0
+
+		
