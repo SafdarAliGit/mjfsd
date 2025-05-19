4fcd4a97cd5db2a33840b441540490cb4b1462dd
SafdarAliGit <safdar211@gmail.com>
2025-05-19 16:46:07 +0500

    updated

--- mjfsd/loom_production/doctype/loom_production/loom_production.js
+++ mjfsd/loom_production/doctype/loom_production/loom_production.js
@@ -14,7 +14,7 @@ frappe.ui.form.on('Loom Production', {
         frm.set_query('sizing_name', 'loom_production_items', function (doc, cdt, cdn) {
             return {
                 filters: [
-                    ["Item", "item_group", "in", ["Beam"]]
+                    ["Item", "item_group", "in", ["Fabric"]]
                 ]
             };
         });
@@ -24,14 +24,13 @@ frappe.ui.form.on('Loom Production', {
 frappe.ui.form.on('Loom Production Items', {
     rpm: function (frm, cdt, cdn) {
         var d = locals[cdt][cdn];
-        var unit_per_rpm = Math.floor(d.rpm* (0.72/ 12) * 7);
+        var unit_per_rpm = Math.floor(d.rpm* (0.72/ 12) * frm.doc.shift_working_hours);
         frappe.model.set_value(cdt, cdn, "unit_per_rpm", unit_per_rpm);
     },
 
-    end_reading: function (frm, cdt, cdn) {
+    actual_reading: function (frm, cdt, cdn) {
         var d = locals[cdt][cdn];
-        var actual_reading = d.end_reading - d.start_reading;
-        frappe.model.set_value(cdt, cdn, "actual_reading", actual_reading);
+        var actual_reading = d.actual_reading;
         var effeciency = Math.floor((actual_reading/d.unit_per_rpm)*100);
         frappe.model.set_value(cdt, cdn, "effeciency", effeciency);
     },
