f47bcc1e2d08f85ed1dd9292aaf57b875cb54982
SafdarAliGit <safdar211@gmail.com>
2025-11-18 19:05:58 +0500

    updated

--- mjfsd/loom_production/doctype/sizing_program/sizing_program.js
+++ mjfsd/loom_production/doctype/sizing_program/sizing_program.js
@@ -512,10 +512,10 @@ function calculate_warp_weight(frm, cdt, cdn){
     let row = locals[cdt][cdn];
     const ends = row.ends;
     const no_of_width = row.no_of_width;
-    const yarn_count = row.yarn_count;
+    const actual_yarn_count = row.actual_yarn_count;
     const wastage_percentage = row.wastage_percentage || 0;
-    if(ends && yarn_count){
-    const warp_wt = ends/no_of_width / 768.10 / yarn_count;
+    if(ends && actual_yarn_count){
+    const warp_wt = ends/no_of_width / 768.10 / actual_yarn_count;
     const warp_weight = warp_wt + (warp_wt * (wastage_percentage/100));
     frappe.model.set_value(cdt, cdn, 'warp_weight', warp_weight);
     }else{
